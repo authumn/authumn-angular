2efb7cb645445f07c5fbee30bee0da969011aa57
Rob Halff <rob.halff@gmail.com>
2018-06-21 01:42:22 +0200

    update version

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -rf dist/
+ng build --prod @authumn/angular-auth
+ng build --prod @authumn/angular-user
+ng build --prod @authumn/angular-templates-bootstrap3
+ng build --prod @authumn/angular-templates-material
+ng build --prod @authumn/angular
