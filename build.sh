b1ef0c25c6229f035564dfa9b5ac77b6b7006688
Rob Halff <rob.halff@gmail.com>
2018-06-21 01:51:31 +0200

    add license

--- build.sh
+++ build.sh
@@ -6,3 +6,24 @@ ng build --prod @authumn/angular-user
 ng build --prod @authumn/angular-templates-bootstrap3
 ng build --prod @authumn/angular-templates-material
 ng build --prod @authumn/angular
+
+cd ./dist/@authumn
+
+cd angular-auth
+npm pack
+
+cd ../angular-auth
+npm pack
+
+cd ../angular-user
+npm pack
+
+cd ../angular-templates-bootstrap3
+npm pack
+
+cd ../angular-templates-material
+npm pack
+
+cd ../angular
+npm pack
+
---
+++ release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd ./dist/@authumn
+
+cd angular-auth
+npm publish
+
+cd ../angular-auth
+npm publish
+
+cd ../angular-user
+npm publish
+
+cd ../angular-templates-bootstrap3
+npm publish
+
+cd ../angular-templates-material
+npm publish
+
+cd ../angular
+npm publish
+
