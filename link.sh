98a78ea9d2d0597909c0a1c19e39a5549aa5fe44
Rob Halff <rob.halff@gmail.com>
2018-06-21 02:53:09 +0200

    add link scripts

---
+++ link.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd ./dist/@authumn
+
+cd angular-auth
+npm link
+
+cd ../angular-auth
+npm link
+
+cd ../angular-user
+npm link
+
+cd ../angular-templates-bootstrap3
+npm link
+
+cd ../angular-templates-material
+npm link
+
+cd ../angular
+npm link
+
---
+++ unlink.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cd ./dist/@authumn
+
+cd angular-auth
+npm unlink
+
+cd ../angular-auth
+npm unlink
+
+cd ../angular-user
+npm unlink
+
+cd ../angular-templates-bootstrap3
+npm unlink
+
+cd ../angular-templates-material
+npm unlink
+
+cd ../angular
+npm unlink
+
