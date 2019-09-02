eafe763ffbb678032f949a6d1357ce719b5719be
Rob Halff <rob.halff@gmail.com>
2019-09-02 03:29:14 +0200

    ng update

--- projects/angular-templates-material/src/lib/material.module.ts
+++ projects/angular-templates-material/src/lib/material.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core'
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatIconModule,
-  MatListModule,
-  MatMenuModule,
-  MatToolbarModule,
-  MatSidenavModule,
-  MatSnackBarModule,
-  MatTabsModule
-} from '@angular/material'
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
